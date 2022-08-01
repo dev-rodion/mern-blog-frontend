48efdadc2b956b5ebecba4803ddfb82207bd86ce
dev-rodion <gudkov.rodion.21@gmail.com>
2022-08-01 11:29:28 +0200

    create and style Header component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { Container } from "@mui/material";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <Container maxWidth="lg">
-        <div className="App">
-          <h1>Hello React JS</h1>
-        </div>
-      </Container>
+      <Header />
+      <Container maxWidth="lg"></Container>
     </>
   );
 }
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+import styles from "./Header.module.scss";
+import { Button, Container } from "@mui/material";
+
+const Header = () => {
+  const isAuth = false;
+
+  const onClickLogout = () => {};
+
+  return (
+    <div className={styles.root}>
+      <Container maxWidth="lg">
+        <div className={styles.inner}>
+          <a href="/" className={styles.logo}>
+            <div>MERN BLOG</div>
+          </a>
+
+          <div className={styles.buttons}>
+            {isAuth ? (
+              <>
+                <a href="/posts/create">
+                  <Button variant="contained">Create a Post</Button>
+                </a>
+                <Button
+                  onClick={onClickLogout}
+                  variant="contained"
+                  color="error"
+                >
+                  Log Out
+                </Button>
+              </>
+            ) : (
+              <>
+                <a href="/login">
+                  <Button variant="outlined">Log In</Button>
+                </a>
+                <a href="/register">
+                  <Button variant="contained">Sing Up</Button>
+                </a>
+              </>
+            )}
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Header;
